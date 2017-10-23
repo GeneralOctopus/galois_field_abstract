8527244c35100c0153678388768626bea18712a6
Bartosz Maciej Jablonka <bartosz.maciej.jablonka@gmail.com>
2017-10-23 23:08:02 +0200

    Add equal operator

--- src/galoise_field.py
+++ src/galoise_field.py
@@ -3,6 +3,8 @@ class GaloisFieldElement(object):
     """implementation"""
     
     def __init__(self, value, modulus):
-        self.value = value
         self.modulus = modulus
+        self.value = value%self.modulus
 
+    def __eq__(self, other):
+        return isinstance(other, GaloisFieldElement) and (self.value == other.value) and (self.modulus == other.modulus) 
--- tests/test_galoise_field.py
+++ tests/test_galoise_field.py
@@ -2,5 +2,16 @@ import pytest
 from KiSD_projekt1.src.galoise_field import *
 
 def test_create_field_element():
-    pass
-#    GaloisFieldElement(5, 4)
+    element1 = GaloisFieldElement(5, 4)
+    element2 = GaloisFieldElement(5, 4)
+    element3 = GaloisFieldElement(4, 4)
+    element4 = GaloisFieldElement(5, 3)
+    element5 = 4
+
+    assert element1 == element2
+    assert element1 != element3
+    assert element1 != element4
+    assert element1 != element5
+
+#def test_elements_generator():
+    
