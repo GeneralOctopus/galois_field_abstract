77b95e93366a6fc87cf1a412aa95370cb7399944
Bartosz Maciej Jablonka <bartosz.maciej.jablonka@gmail.com>
2017-10-23 23:33:51 +0200

    Add generator class

--- src/galoise_field.py
+++ src/galoise_field.py
@@ -2,9 +2,18 @@
 class GaloisFieldElement(object):
     """implementation"""
     
-    def __init__(self, value, modulus):
+    def __init__(self, modulus, value):
         self.modulus = modulus
         self.value = value%self.modulus
 
     def __eq__(self, other):
         return isinstance(other, GaloisFieldElement) and (self.value == other.value) and (self.modulus == other.modulus) 
+
+
+class GaloisFieldGenerator(object):
+    
+    def __init__(self, modulus):
+        self.modulus = modulus
+
+    def generate(self, value):
+        return GaloisFieldElement(self.modulus, value)
--- tests/test_galoise_field.py
+++ tests/test_galoise_field.py
@@ -1,11 +1,12 @@
 import pytest
 from KiSD_projekt1.src.galoise_field import *
 
+
 def test_create_field_element():
-    element1 = GaloisFieldElement(5, 4)
+    element1 = GaloisFieldElement(5, 9)
     element2 = GaloisFieldElement(5, 4)
     element3 = GaloisFieldElement(4, 4)
-    element4 = GaloisFieldElement(5, 3)
+    element4 = GaloisFieldElement(3, 4)
     element5 = 4
 
     assert element1 == element2
@@ -13,5 +14,18 @@ def test_create_field_element():
     assert element1 != element4
     assert element1 != element5
 
-#def test_elements_generator():
+
+def test_elements_generator():
+    modulus = 2**8
+    generator = GaloisFieldGenerator(modulus)
+    element1 = generator.generate(7)
+    element2 = generator.generate(256)
+    element3 = generator.generate(261)
+
+    assert element1.modulus == modulus
+    assert element1.value == 7
+    assert element2.modulus == modulus
+    assert element2.value == 0
+    assert element3.modulus == modulus
+    assert element3.value == 5
     
